751aba71f993d50612c282d2256ae1b37440b638
Thalles1946 <78450993+Thalles1946@users.noreply.github.com>
2021-05-13 14:05:36 -0300

    Update 1.3.py
    With that update (more are going to come) you can now create passwords with letters, no more just with numbers.

--- lista=[1,3,5,7].py
+++ lista=[1,3,5,7].py
@@ -1,29 +1,29 @@
 n="admin"
-s=123456
+s=str("123456")
 nd=0
 sd=0
 print("bem vindo ao cofre, se voce não tiver login, responda 'não' a pergunta abaixo")
 p1=str(input("Voce tem login? "))
 if p1=="não":
     n=str(input("digite o seu nome para o novo login: "))
-    s=int(input("agora digite sua senha: "))
-    s1=int(input("repita a senha: "))
+    s=str(input("agora digite sua senha: "))
+    s1=str(input("repita a senha: "))
     while s1!=s:
-        s1=int(input("as senhas não coincidem, digite novamente a senha s1: "))
+        s1=str(input("as senhas não coincidem, digite novamente a senha s1: "))
     print("login criado com sucesso")
     nd=str(input("para entrar no sistema digite seu nome: "))
-    sd=int(input("agora, digite sua senha: "))
+    sd=str(input("agora, digite sua senha: "))
     while nd!=n or sd!=s:
         print ("dados invalidos, digite novamente")
         nd=str(input("para entrar no sistema digite seu nome: "))
-        sd=int(input("agora, digite sua senha: "))
+        sd=str(input("agora, digite sua senha: "))
     print ("acesso permitido")
 elif p1=="sim":
     print("digite seu nome e senha: ")
     nz=str(input())
-    sz=int(input())
+    sz=str(input())
     while nz!=n and sz!=s:
         print("dados invalidos, digite novamente")
         nz=str(input("para entrar no sistema digite seu nome: "))
-        sz=int(input("agora, digite sua senha: "))
+        sz=str(input("agora, digite sua senha: "))
     print("acesso permitido")
